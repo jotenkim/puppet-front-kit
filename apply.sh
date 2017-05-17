a9b84b7f2daa8aa11c68229237bf66e9bea511ad
Kim <wertyy@gmail.com>
2017-05-17 03:37:50 +0000

    bash to actually install atom packages

---
+++ apply.sh
@@ -0,0 +1,15 @@
+apm install atom-beautify
+apm install atom-beautify
+apm install color-picker
+apm install atom-bootstrap3
+apm install emmet
+apm install jquery-snippets
+apm install linter
+apm install linter-ui-default
+apm install linter-js-standard
+apm install minimap
+apm install pigments
+apm install merge-conflicts
+apm install merge-conflicts
+sudo puppet apply -e --modulepath puppet/modules/ 'class{setting-module:}'
+
---
+++ start.sh
@@ -0,0 +1,8 @@
+setxkbmap fi
+sudo apt-get update
+sudo apt-get -y install puppet git
+git clone https://github.com/jotenkim/puppet-front-kit
+cd puppet-front-kit
+sudo apply -e --modulepath puppet/modules/ 'class{puppet-front-kit:}'
+cd ..
+bash apply.sh
