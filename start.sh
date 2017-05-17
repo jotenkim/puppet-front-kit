a4ee02104681dc7595243f5c593bb402890adbc5
jotenkim <wertyy@gmail.com>
2017-05-17 05:07:24 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@ setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install puppet git
 git clone https://github.com/jotenkim/puppet-front-kit
-cd puppet-front-kit
-sudo apply -e --modulepath puppet/modules/ 'class{puppet-front-kit:}'
-cd ..
+cd puppet-front-kit/
+sudo apply -e --modulepath puppet/modules/ 'class{atom-puppet:}'
 bash apply.sh
