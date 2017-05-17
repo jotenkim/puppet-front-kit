f7d59d304097946ee0ca1eb28d41e763ecb1dad1
jotenkim <wertyy@gmail.com>
2017-05-17 05:07:52 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get -y install puppet git
 git clone https://github.com/jotenkim/puppet-front-kit
 cd puppet-front-kit/
-sudo apply -e --modulepath puppet/modules/ 'class{atom-puppet:}'
+sudo apply -e --modulepath puppet/modules/ 'class {atom-puppet:}'
 bash apply.sh
